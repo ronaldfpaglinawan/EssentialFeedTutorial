ef3f4e0dd3f985cd51654fb94ed3b8322eae0eda
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2024-12-14 16:44:16 +1300

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ronald Fornis Paglinawan on 14/12/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
